d383ff3413f955732074e648e6af27c9fe38b2c0
Mikhail Akimov <rovinj.akimov@gmail.com>
2020-12-01 19:00:58 +0300

    oops, add packages

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="streamz_gcp_pubsub",
     version="0.1.0",
+    packages=find_packages(),
     install_requires=[
         "streamz @ git+https://github.com/python-streamz/streamz.git",
         "google-cloud-pubsub",
