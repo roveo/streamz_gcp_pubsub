66ff68c6b20a83f0e46e6175460809f21e81a668
Mikhail Akimov <rovinj.akimov@gmail.com>
2020-12-30 12:13:55 +0300

    update version and tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamz_gcp_pubsub",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "streamz @ git+https://github.com/python-streamz/streamz.git",
